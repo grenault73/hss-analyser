3c320b0bb33edcab7413bcedc3876d9ac4937516
grenault73 <grenault73@gmail.com>
2018-11-22 14:42:36 +0100

    load: fetch HTTP manifests

--- src/load/fetchManifest.js
+++ src/load/fetchManifest.js
@@ -1,24 +1,35 @@
 import https from "https";
+import http from "http";
 
 export default function fetchManifest(url) {
   return new Promise((resolve) => {
-      const getRequest = https.get(url, (res) => {
-        res.setEncoding("utf8");
-        let body = "";
-        res.on("data", data => {
-          body += data;
-        });
-        res.on("end", () => {
-          resolve({
-            manifest: body,
-          });
-        });
-        res.on("error", (err) => {
-          resolve({ err })
-        })
+    const protocol = (() => {
+      if (url.startsWith("https://")) {
+        return https;
+      } else if (url.startsWith("http://")) {
+        return http;
+      } else {
+        resolve({ err: new Error("Unknown protocol.") })
+      }
+    })();
+
+    const getRequest = protocol.get(url, (res) => {
+      res.setEncoding("utf8");
+      let body = "";
+      res.on("data", data => {
+        body += data;
       });
-      getRequest.on("error", (err) => {
-        resolve({ err });
+      res.on("end", () => {
+        resolve({
+          manifest: body,
+        });
+      });
+      res.on("error", (err) => {
+        resolve({ err })
       })
+    });
+    getRequest.on("error", (err) => {
+      resolve({ err });
+    })
   })
 }
